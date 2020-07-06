be7271bdb152b163a7fe497bb455dae45ff3fc05
Esther Min <estherjmin@gmail.com>
2020-07-06 16:45:48 -0400

    setup routes for express GET

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
 // Express setup
 var app = express();
@@ -9,7 +9,20 @@ var PORT = 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-server.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+// Routes
+// =============================================================
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+  
+app.get("*", function(req, res) {
+res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// =============================================================
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
