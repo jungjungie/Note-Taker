f9285b0c47fb5d6e6ef0eb74a5353dc75d33e41f
Esther Min <estherjmin@gmail.com>
2020-07-07 15:17:52 -0400

    Created POST API route to take new note and save it to db.json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Express setup
 var app = express();
 var PORT = 8080;
 
-// Sets up the Express app to handle data parsing
+let notesArr = [];
+
+// Express setup to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -15,14 +18,30 @@ app.use(express.static('public'));
 // =============================================================
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
-  
+});
+
+// Receives a new note to save, adds it to db.json, and returns the new note to the client
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+
+    notesArr.push(newNote);
+    console.log(notesArr);
+
+    fs.writeFile("db/db.json", JSON.stringify(notesArr), function(err) {
+        if (err) throw err;
+        console.log('db.json was successfully written!')
+    })
+
+    // return res.json(newNote);
+});
+
 app.get("*", function(req, res) {
-res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // =============================================================
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
