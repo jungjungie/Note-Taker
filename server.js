a90a09fb90e7b3406475158367a31d862285d63a
Esther Min <estherjmin@gmail.com>
2020-07-07 17:47:33 -0400

    Setup DELETE API route to remove chosen note from db.json and rewrite to db with remaining notes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/api/notes", function(req, res) {
 
 // Receives a new note to save, adds it to db.json, and returns the new note to the client
 app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
+    let newNote = req.body;
     notesArr.push(newNote);
     console.log(notesArr);
 
@@ -45,6 +45,32 @@ app.post("/api/notes", function(req, res) {
     })
 
     return res.json(newNote);
+});
+
+// Deletes the selected note from db.json and then rewrites the remaining notes in the array to db.json 
+app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+        if (err) throw err;
+
+        let savedNotesArr = JSON.parse(data);
+        let chosenId = req.params.id;
+    
+        for (let i=0; i < savedNotesArr.length; i++) {
+            if (chosenId == savedNotesArr[i].id) {
+                let chosenIndex = savedNotesArr.indexOf(savedNotesArr[i]);
+
+                savedNotesArr.splice(chosenIndex, 1);
+
+                fs.writeFile("db/db.json", JSON.stringify(savedNotesArr), function(err) {
+                    if (err) throw err;
+                    console.log('Your savedNotesArr was successfully written to db.json!')
+                })
+
+                notesArr = savedNotesArr;
+                return res.json(savedNotesArr);
+            }
+        }
+    })
 });
 
 app.get("*", function(req, res) {
