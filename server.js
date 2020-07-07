ea53dc65212fcb65938e91b0977bb31c37bb4775
Esther Min <estherjmin@gmail.com>
2020-07-07 16:06:37 -0400

    Setup GET API route to read db.json and added ids to each note to render correct display when clicked

--- server.js
+++ server.js
@@ -12,28 +12,39 @@ let notesArr = [];
 // Express setup to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 // Routes
 // =============================================================
+app.use(express.static('public'));
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+  
+// Reads db.json and returns all saved notes as JSON
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 // Receives a new note to save, adds it to db.json, and returns the new note to the client
 app.post("/api/notes", function(req, res) {
     var newNote = req.body;
-    console.log(newNote);
-
     notesArr.push(newNote);
     console.log(notesArr);
 
-    fs.writeFile("db/db.json", JSON.stringify(notesArr), function(err) {
-        if (err) throw err;
-        console.log('db.json was successfully written!')
+    let id = 1;
+
+    notesArr.forEach(item => {
+        item.id = id;
+        id++
     })
 
-    // return res.json(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(notesArr), function(err) {
+        if (err) throw err;
+        console.log('Your notesArr was successfully written to db.json!')
+    })
+
+    return res.json(newNote);
 });
 
 app.get("*", function(req, res) {
